e05ae04f5a29383185bf78799fc80637bc043ccc
Анна <anna.kuvakina91@gmail.com>
2024-07-11 20:40:24 +0300

    css

--- src/components/AuthNav/AuthNav.jsx
+++ src/components/AuthNav/AuthNav.jsx
@@ -1,11 +1,12 @@
 import { NavLink } from "react-router-dom";
+import css from "./AuthNav.module.css";
 
 const AuthNav = () => {
   return (
-    <>
+    <div className={css.nav}>
       <NavLink to="/register">Register</NavLink>
       <NavLink to="/login">Log In</NavLink>
-    </>
+    </div>
   );
 };
 

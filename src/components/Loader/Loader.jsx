e2dca264cb983d51488698102864fe522cfc11e6
Анна <anna.kuvakina91@gmail.com>
2024-07-22 22:54:37 +0300

    loader-css

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,16 +1,19 @@
-import { Audio } from "react-loader-spinner";
+import { Hearts } from "react-loader-spinner";
+import css from "./Loader.module.css";
 
 const Loader = () => {
   return (
-    <Audio
+    <div className={css.loader_container}>
+    <Hearts
       height="80"
       width="80"
-      radius="9"
-      color="green"
-      ariaLabel="three-dots-loading"
-      wrapperStyle
-      wrapperClass
-    />
+      color="#490537"
+      ariaLabel="hearts-loading"
+      wrapperStyle={{}}
+      wrapperClass=""
+      visible={true}
+      />
+      </div>
   );
 };
 

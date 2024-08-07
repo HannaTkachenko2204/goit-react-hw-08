5a111f8b4867de15da5406b6b75d7d4174405d47
Анна <anna.kuvakina91@gmail.com>
2024-08-07 14:20:06 +0300

    tablet nav

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -22,11 +22,11 @@ const CustomDropdownItem = styled(Dropdown.Item)`
 
 const Navigation = () => {
   const isLoggedIn = useSelector(selectIsLoggedIn);
-  const [isMobile, setIsMobile] = useState(window.innerWidth < 1025);
+  const [isMobile, setIsMobile] = useState(window.innerWidth < 768);
   const [showDropdown, setShowDropdown] = useState(false);
 
   useEffect(() => {
-    const handleResize = () => setIsMobile(window.innerWidth < 1025);
+    const handleResize = () => setIsMobile(window.innerWidth < 768);
     window.addEventListener("resize", handleResize);
     return () => window.removeEventListener("resize", handleResize);
   }, []);
